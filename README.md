f710ae9c988cbefd5cc44f402e78b0455114c820
Michael Seborowski <seborowskim13@live.com>
2026-03-26 18:40:15 -0400

    Updated handle_helper.ps1 and README.md
    Updated handle_helper.ps1 with inline comments to explain the process better
    
    Also updated the readme file with a semi-final command to run as well as requirements

--- README.md
+++ README.md
@@ -1,8 +1,62 @@
-Test Mod Loader for EVE Frontier
+[Test Mod Loader for EVE Frontier]
 
+<#
+.SYNOPSIS
+    Monitors target process for open file handles related to EVE Frontier's exefile.
+    Attempts to patch code.ccp with modified files from the mod folder during a very specific/tight window.
+
+.AUTHOR Author of this toolkit and script
+    ProtoDroidBot (EVE Frontier and GitHub name)
+
+.DESCRIPTION
+    - Monitors a process for open file handles related to EVE Frontier's exefile.
+    - Also compiles and loads a mod folder defined by the $mod_folder parameter in order to inject code into the game.
+
+.REQUIREMENTS Requirements for using this script
+    - Run PowerShell as Administrator (Handle.exe requires elevated privileges)
+    - EVE Frontier (https://www.evefrontier.com/)
+    - Sysinternals Handle.exe (https://learn.microsoft.com/sysinternals/downloads/handle)
+    - Python version 3.12 (ideally Python 3.12rc2) and python3 binary copied/renamed to python312.exe
+    - 7-zip - (https://www.7-zip.org/)
+    - decompiled python files / Pylingual extracts from the code.ccp file. note that the directory structure MUST be retained when editing. YMMV otherwise!
+    ! - Note that I shouldn't provide the decompiled python files from the code.ccp file due to legal reasons, but you can obtain them yourself using a tool like Pylingual
+    ! - Some editing required on the decompiled python files is needed in order to get them to compile because of the way Python 3.12 bytecode works.
+
+.PARAMETER ProcessName
+    $ProcessName should be set to the exefile.exe of EVE Frontier
+
+.PARAMETER pythonExe
+    $pythonExe is the path to the python executable (ideally python 3.12rc2) that will be used to compile the mod files and run the loadmod.py script.
+
+.PARAMETER HandleExe
+    $HandleExe is the path to the Sysinternals Handle.exe utility.
+
+.PARAMETER SevenZExe
+    $SevenZExe is the path to the 7-zip executable, used for extracting and repacking the code.ccp file.
+
+.PARAMETER mod_folder
+    $mod_folder is the path to the folder containing the mod files. This folder will be monitored for .py files to compile and load into the game.
+
+.PARAMETER targetserver
+    $targetserver is the server folder name of the EVE Frontier installation, used for targeting the correct code.ccp file for modding.
+
+.PARAMETER recompile
+    $recompile is a boolean flag that determines whether the mod files should be recompiled on each run. Set to $true to enable recompilation, or $false to skip it.
+.PARAMETER precached
+    $precached is a boolean flag that determines whether the mod files should be precached on each run. Set to $true to enable precaching, or $false to skip it.
+.PARAMETER interval
+    $Interval is a int value that determines the sleep interval (in seconds) between checks for the process and file handles. Default is 1.5 seconds. Adjust this number as needed.
+#>
+
+
+
+
+Example Command line: Requires PowerShell 7
+Set-ExecutionPolicy Bypass; pwsh .\handle_helper.ps1 -ProcessName exefile.exe -pythonExe C:\Users\sebor\AppData\Local\Programs\Python\Python312\python.exe -HandleExe .\handle.exe -SevenZExe "C:\Program Files\7-Zip\7z.exe" -mod_folder C:\Users\sebor\OneDrive\Software\GitHub\lemegetonmodloader-POC1\mods\SSU_to_Industry -targetserver utopia -recompile $true -Interval 1.5 -precached false
+
+Previous Command lines:
+Very old and was pre-PowerShell 7 (PowerShell 5) and this did NOT support parallel ForEach-Object command execution that I needed
 ```Set-ExecutionPolicy Bypass; .\handle_helper.ps1 -ProcessName exefile.exe -pythonExe C:\Users\username\AppData\Local\Programs\Python\Python312\python.exe -HandleExe .\handle.exe -SevenZExe "C:\Program Files\7-Zip\7z.exe" -mod_folder C:\Users\username\OneDrive\Software\pymemstuff\lemegetonmodloader\mods\SSU_to_Industry -targetserver utopia -recompile $true```
 
-
-
-Example Command line 2: Requires powershell 7
+Requires PowerShell 7
 ```Set-ExecutionPolicy Bypass; pwsh .\handle_helper.ps1 -ProcessName exefile.exe -pythonExe C:\Users\sebor\AppData\Local\Programs\Python\Python312\python.exe -HandleExe .\handle.exe -SevenZExe "C:\Program Files\7-Zip\7z.exe" -mod_folder C:\Users\sebor\OneDrive\Software\GitHub\lemegetonmodloader-POC1\mods\SSU_to_Industry -targetserver utopia -recompile $true -Interval 0.7 -precached false```
--- handle_helper.ps1
+++ handle_helper.ps1
@@ -1,18 +1,24 @@
 <#
 .SYNOPSIS
-    Monitors a process for open file handles related to EVE Frontier's exefile.
+    Monitors target process for open file handles related to EVE Frontier's exefile.
+    Attempts to patch code.ccp with modified files from the mod folder during a very specific/tight window.
+
+.AUTHOR Author of this toolkit and script
+    ProtoDroidBot (EVE Frontier and GitHub name)
 
 .DESCRIPTION
     - Monitors a process for open file handles related to EVE Frontier's exefile.
     - Also compiles and loads a mod folder defined by the $mod_folder parameter in order to inject code into the game.
 
-.REQUIREMENTS
+.REQUIREMENTS Requirements for using this script
     - Run PowerShell as Administrator (Handle.exe requires elevated privileges)
-    - EVE Frontier (https://learn.microsoft.com/sysinternals/downloads/handle)
+    - EVE Frontier (https://www.evefrontier.com/)
     - Sysinternals Handle.exe (https://learn.microsoft.com/sysinternals/downloads/handle)
     - Python version 3.12 (ideally Python 3.12rc2) and python3 binary copied/renamed to python312.exe
     - 7-zip - (https://www.7-zip.org/)
     - decompiled python files / Pylingual extracts from the code.ccp file. note that the directory structure MUST be retained when editing. YMMV otherwise!
+    ! - Note that I shouldn't provide the decompiled python files from the code.ccp file due to legal reasons, but you can obtain them yourself using a tool like Pylingual
+    ! - Some editing required on the decompiled python files is needed in order to get them to compile because of the way Python 3.12 bytecode works.
 
 .PARAMETER ProcessName
     $ProcessName should be set to the exefile.exe of EVE Frontier
@@ -37,7 +43,7 @@
 .PARAMETER precached
     $precached is a boolean flag that determines whether the mod files should be precached on each run. Set to $true to enable precaching, or $false to skip it.
 .PARAMETER interval
-    $Interval is a int value that determines the sleep interval (in seconds) between checks for the process and file handles. Default is 33 seconds.
+    $Interval is a int value that determines the sleep interval (in seconds) between checks for the process and file handles. Default is 1.5 seconds. Adjust this number as needed.
 #>
 param(
     [Parameter(Mandatory = $true)]
@@ -56,16 +62,19 @@ param(
     [string]$recompile,
     [Parameter(Mandatory = $true)]
     [string]$precached,
-    [float]$Interval = 33
+    [float]$Interval = 1.5
 )
 
 # Path to Sysinternals Handle.exe
- # Change this to your actual path
 
 if (-not (Test-Path $HandleExe)) {
     Write-Error "handle64.exe not found at $HandleExe"
     exit 1
 }
+
+# Python 3.12 script compiler and code.ccp packing
+# Sometimes this process errors out because of file locks on the code.ccp file
+# If this happens, just run the script again after deleting the code.ccp.tmp file and it should work on the next try. 
 
 function PythonScriptsEF {
     if ($recompile -eq $true){
@@ -76,6 +85,9 @@ function PythonScriptsEF {
                     {
                         & $pythonExe "-m" "py_compile" $_.FullName
                     }
+                }
+            Write-Host "cleaning up $mod_folder and moving stuff to the proper place" -ForegroundColor Green
+            Get-ChildItem -Recurse $mod_folder | ForEach-Object {
                 if ($_.FullName -match ".cpython-312.pyc")
                     {
                         $newpath = Join-Path $_.Directory ($_.Name -replace '.cpython-312.pyc', '.pyc'); 
@@ -96,18 +108,26 @@ function PythonScriptsEF {
         }
         else {
             Write-Host ":( Recompiling mod files in $mod_folder, but python312.exe is not available! ignoring" -ForegroundColor Red
+            return 1
         }
     }
     return 0
 }
 
+# Function to monitor file handles for the specified process, then attempts to inject mod files once the target file handle is detected and released by the game.
+# Do note that the injection method used here is very rudimentary and may not work in all cases.
+# It relies on the game releasing the manifest.dat file handle after loading it, which may not always happen in a predictable manner.
+# Also this script requires PowerShell 7 because of the use of Foreach-Object -Parallel, which is used to attempt to time the injection of the mod files during the window of time where the game has released the manifest.dat file handle checking on code.ccp but exefile.exe (Game process) has not yet reloaded it.
 function HandleHelper(){
     Write-Host "Monitoring file handles for process: $ProcessName" -ForegroundColor Cyan
     Write-Host "Press Ctrl+C to stop." -ForegroundColor Yellow
     while ($true) {
         try{
+            # Get the process ID of the target process using WMI.
             $test = Get-WmiObject Win32_Process -Filter "Name = 'exefile.exe'" | Select-Object Name, ProcessId, CommandLine
             $pids = $test.ProcessId
+
+            # PowerShell stopwatch object to time the file handle release.
             $sw = [System.Diagnostics.Stopwatch]::StartNew()
             if ($test -ne $null) {
                 Write-Host "caught process" -ForegroundColor Yellow
@@ -121,9 +141,15 @@ function HandleHelper(){
                     Sort-Object -Unique
                 $currentHandles
                 if ($currentHandles -match "manifest.dat"){
+                    # After grabbing the time elapsed, we can then reuse this time value to attempt to time the injection of the mod files 
                     $sw.Stop()
                     Write-Output "Total time elapsed: $($sw.Elapsed.TotalSeconds) seconds"
-                    Write-Host "attempt" -ForegroundColor Yellow
+
+                    # This bit grabs the command line from $test and subsstring splits it into variables to be used for starting the game process again after killing it to attempt to force the game to reload the code.ccp file.
+                    # to inject the mod files during the window of time where the manifest.dat file handle is released but the game process is still running, which should cause the game to load the modded code.ccp file instead of the original one.
+                    # Also makes it so that you dont have to start the game through the command line every time, which is a QoL improvement for testing mod files.
+                    # Also starts the game without /noconsole for debugging purposes.
+                    Write-Host "attempting to grab command line argumentsto pipe back into a separate process" -ForegroundColor Yellow
                     $exe, $b, $c, $d, $e, $f, $g, $h, $i, $j = ($test.CommandLine).Split("/", 10)
                     $c = $c.Substring(0, 0) + "/"+ $c.Substring(0).Replace('"', "")
                     $d = $d.Substring(0, 0) + "/"+ $d.Substring(0).Replace('"', "")
@@ -138,6 +164,10 @@ function HandleHelper(){
 
                     Write-Host "Attempting to inject mod files..." -ForegroundColor Yellow
                     
+                    # This bit of scriptblock required me to use PowerShell 7 because of -Parallel, which is used here to time the injection of the mod files with the compilation of the code.ccp file.
+                    # Idea is to compile the mod file and patch the code.ccp file in the specified time window where the manifest.dat file handle is released and the game process hasn't loaded code.ccp yet.
+                    # This is the meat of the exploit.
+                    # If the timing is off even slightly, it will either cause invalid file header errors in code.ccp, import errors, or best case the game complains about modified files.
                     1..2 | Foreach-Object -Parallel {
                         if ($_ -eq 1){
                             ($using:sw.Elapsed.TotalSeconds)
@@ -164,11 +194,25 @@ function HandleHelper(){
 }   
 
 # Store previous handle list for comparison
+# Might not be needed anymore. Keeping it here just in case. :)
 $SevenZExe = Get-Item -LiteralPath $SevenZExe
+
+# Prints the PowerShell version table to the console for debugging purposes. This is useful for verifying that the script is running on PowerShell 7, which is required for the use of Foreach-Object -Parallel in the HandleHelper function.
 $PSVersionTable
-PythonScriptsEF
-HandleHelper
+
+# Actually run the script functions
+$didthisrun = PythonScriptsEF
+if ($didthisrun -ne 0){
+    Write-Host "Error during Python script compilation. Exiting." -ForegroundColor Red
+    exit 1
+}
+else{
+    HandleHelper
+}
 
 
 
+# VERY OLD TESTING CMD.EXE BATCH SCRIPT BELOW, IGNORE. Or dont, I am not your dad.
+# If I recall correctly, this was used to test and verify the file handles of the game process, and killing the file handle.
+# Doing this caused huge game instability.
 #for /F "tokens=3,6 delims=: " %I IN ('C:\test\handle64.exe -accepteula "C:\CCP\EVE Frontier\utopia\code.ccp"') DO "C:\test\handle64.exe" -c %J -y -p %I
