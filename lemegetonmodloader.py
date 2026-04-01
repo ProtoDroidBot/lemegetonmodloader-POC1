c625309c09ffbb2846b31500f93261bf75c6a2df
Michael Seborowski <seborowskim13@live.com>
2026-03-31 20:40:26 -0400

    Hackathon 2026 last ditch attempt
    Futile attempt at refactoring the codebase to use less dependencies, will improve this post hackathon

--- lemegetonmodloader.py
+++ lemegetonmodloader.py
@@ -1,31 +1,47 @@
-import asyncio
+
 import psutil
 import time
 from mods import loadmod
 import os
+import subprocess
+import asyncio
+from pathlib import Path
 
-async def compiler(work, modfolder):
-    print("Compiler running")
-    loadmod.modloader(work, modfolder)
+async def gamelauncher(work, commandline):
+    try:
+        await asyncio.sleep(2)
+        subprocess.run([work, commandline[2], commandline[3], commandline[4], commandline[5], commandline[6], commandline[7], commandline[8], commandline[9], commandline[10]])
 
-async def gamelauncher(commandline):
-    asyncio.sleep(2.5)
-    print("Game launch routines running")
 
-async def main(work, commandline):
-    print(os.getcwd())
-    await asyncio.gather(compiler(work, os.getcwd()), gamelauncher(commandline))
+    except Exception as e:
+        print(f"Game Subprocess error:  ", e)
 
-if __name__ == "__main__":
+#def main(work, commandline):
+    #print(os.getcwd())
+    #await asyncio.gather(compiler(work, os.getcwd()), gamelauncher(work, commandline))
+
+async def main():
     while True:
-        time.sleep(1)
+        time.sleep(2)
         for process in psutil.process_iter(['pid', 'name', 'cmdline']):
         # The cmdline() method returns a list of the command line arguments
         # You can filter based on process name or other criteria
             if 'exefile' in process.name() and process.cmdline():
                 print(f"PID: {process.info['pid']}, Name: {process.info['name']}")
-                print(f"Command line: {process.info['cmdline'][1]}")
-                command = process.info['cmdline']
-                asyncio.run(main(command[1], command))
-                time.sleep(10)
+                print(f"Command line: {process.cmdline()}")
+                command = process.cmdline()
+                print(f"Game EXE: {process.exe()}")
+                game_exe = process.exe()
+                process.kill()
+
+                results = asyncio.gather(
+                    gamelauncher(game_exe, command),
+                    loadmod.modloader(Path(game_exe), os.getcwd())
+                )
+                print(f"finished at {time.strftime('%X')}")
+                print(f"Results: {results}")
                 break
+
+if __name__ == "__main__":
+    print(f"Awaiting EVE Frontier exefile.exe execution. Please launch the game via the EVE Frontier Launcher.")
+    asyncio.run(main())
--- mods/loadmod.py
+++ mods/loadmod.py
@@ -1,6 +1,7 @@
 import subprocess
 import psutil
-import sys
+import time
+import asyncio
 import subprocess
 import shutil
 from pathlib import Path
@@ -22,21 +23,21 @@ def load_mods_into_path_modules(target_path, mod_dir):
     try:
         # Copy the file to the destination directory
 
-        mod_zip = str(mod_dir + "\\code.ccp")
-        remove_file_from_7zip(mod_zip, replace_target)
+        #mod_zip = Path.joinpath(mod_dir, "code.ccp")
+        remove_file_from_7zip(mod_dir, replace_target)
         # Insert the file into the existing ZIP archive using append mode ('a')
-        with zipfile.ZipFile(mod_zip, 'a', compression=zipfile.ZIP_STORED) as zipf:
+        with zipfile.ZipFile(mod_dir, 'a', compression=zipfile.ZIP_STORED) as zipf:
 
             zipf.write(import_target_input, arcname=str(replace_target.replace(mod_dir, '') + "/"))
-            print(f"Added {import_target_input} to {mod_zip}")
+            print(f"Added {import_target_input} to {mod_dir}")
 
     except Exception as e:
         print(f"An unexpected error occurred in load_mods_into_path_modules: {e}; please inform ProtoDroidBot and create an issue on GitHub at: ")
 
 def remove_file_from_7zip(archive_path, file_to_remove):
     #seven_zip_executable = 'C:\\Program Files\\7-Zip\\7z.exe' 
-    seven_zip_executable = Path(f'{SevenZExe}')
-    print("Attempting file removal")
+    seven_zip_executable = Path(os.getcwd(), '7za.exe')
+    print(f"Attempting file replace/removal with 7za.exe from: {seven_zip_executable}")
     # The command is '7z d archive.zip file_to_delete'
     command = [
         seven_zip_executable,
@@ -55,32 +56,33 @@ def remove_file_from_7zip(archive_path, file_to_remove):
     except Exception as err:
         print(f"There was another unresolved error in the remove_file_from_7zip function: {err}; please inform ProtoDroidBot and create an issue on GitHub at: ")
 
-def modloader(gamepath, mod_folder):
+async def modloader(gamepath, mod_folder):
     try:
-        game = Path(gamepath)
-        print(game)
-        mod_dir_original = Path(mod_folder)
-        mod_dir = mod_dir_original
-        print(mod_dir, mod_dir_original)
-        mod_dir.joinpath(mod_dir, 'code.ccp')
+        game_dir = Path(gamepath)
+        #print(game_dir.parent.parent)
+        mod_dir = Path(mod_folder, 'mods')
+        print(mod_folder, mod_dir)
+        codeccp = mod_dir.joinpath(mod_dir, 'code.ccp')
+        #game_dir = game_dir[:-2].with_segments()
         #{gamepath}\\{targetserver}\\code.ccp
-        game.joinpath(game, 'code.ccp')
-        shutil.copy(Path(game), mod_dir)
+        game_dir2 = Path.joinpath(game_dir.parent.parent, 'code.ccp')
+        #print(game_dir2)
+        shutil.copy(Path(game_dir2), mod_dir)
         #mod_dir_abs = str(f'{mod_folder}')
         #compiled_directory_path = Path(f'{mod_dir_abs}')
 
-        for root, dirs, files in os.walk(mod_dir_original):
-            for file in files:
-                if file == "code.ccp":
-                    continue
-                elif file.endswith(".pyc"):
-                    full_file_path = os.path.join(root, file)
-                    load_mods_into_path_modules(full_file_path, mod_dir)
-                else:
-                    continue
+        #for root, dirs, files in os.walk(mod_dir):
+            #for file in files:
+                #if file == "code.ccp":
+                    #continue
+                #elif file.endswith(".pyc"):
+                    #full_file_path = os.path.join(root, file)
+                    #load_mods_into_path_modules(full_file_path, codeccp)
+                #else:
+                    #continue
                 #Path(f"{mod_folder}\\code.ccp")
-            
-        shutil.copy(mod_dir, game)
+        time.sleep(6.5)
+        shutil.copy(codeccp, game_dir2)
         print("Code.ccp file overwritten, game should launch with the modified file\n")
     except Exception as e:
         print(f"Failed to load mods due to this error: {e}. :( \n")
